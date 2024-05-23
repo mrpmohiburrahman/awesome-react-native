15105642024ddb7a64515c41231683d547cc4d48
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-05-23 12:19:38 +0600

    📝 add styling

--- sidebars.js
+++ sidebars.js
@@ -24,6 +24,7 @@ module.exports = {
             "components/geolocation",
             "components/internationalization",
             "components/buildAndDevelopment",
+            "components/styling",
             "components/bottomsheetfooter",
             "components/bottomsheettextinput",
           ],
